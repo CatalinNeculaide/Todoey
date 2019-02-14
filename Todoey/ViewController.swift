7a389464d8fdfb6ca3585a86d8c210e6e5e698ce
Catalin Neculaide <catalinneculaide@catalins-macbook-pro.local>
2019-02-14 17:15:54 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Catalin Neculaide on 14/02/2019.
+//  Copyright © 2019 Catalin Neculaide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
